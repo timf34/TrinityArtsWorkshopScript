8061f172568dfbb62bbeb4b0961dda5afd8e6b02
timf34 <farrelti@tcd.ie>
2024-01-31 10:40:06 +0000

    working on any eventbrite page

--- script.py
+++ script.py
@@ -1,22 +1,30 @@
-import requests
 import time
-
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.chrome.service import Service
-from typing import List
-
 
 options = EdgeOptions()
 service = Service(EdgeChromiumDriverManager().install())
 driver = webdriver.Edge(service=service, options=options)
 
-driver.get("https://www.eventbrite.com/e/introduction-to-thai-yoga-massage-tickets-776225129257?aff=erelexpmlt&keep_tld=1")
-time.sleep(4)
+event_url = "https://www.eventbrite.com/e/introduction-to-project-management-tickets-55968945623?aff=erelexpmlt&keep_tld=1"
+driver.get(event_url)
+time.sleep(4)  # Wait for the page to load
 
-checkout_button = driver.find_element(By.XPATH, "/html/body/div/div/section/div[2]/button")
+try:
+    # Locate the iframe by a partial match of its 'data-automation' attribute
+    iframe = driver.find_element(By.CSS_SELECTOR, "iframe[data-automation*='checkout-widget-iframe']")
+    driver.switch_to.frame(iframe)
 
-checkout_button.click()
+    # Update the selector for the checkout button based on a more consistent attribute
+    checkout_button = driver.find_element(By.CSS_SELECTOR, "button[data-automation='eds-modal__primary-button']")
+    checkout_button.click()
+
+    time.sleep(1300)  # Wait to manually complete the process
+
+except Exception as e:
+    print(f"Error: {e}")
+
+# Keep the browser window open for manual completion of the process
